de5c46546345dc3bd33c54dff6a9ce72f7be6688
theschmocker <jacobcs1996@gmail.com>
2021-07-25 20:24:11 -0500

    adds doom emacs config to repo
    adds clarifying comment about ts server env variable

---
+++ emacs/.doom.d/custom.el
@@ -0,0 +1,47 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2c2525" "#fd6883" "#adda78" "#f9cc6c" "#85dacc" "#85dacc" "#85dacc" "#fff1f3"])
+ '(custom-safe-themes
+   '("733ef3e3ffcca378df65a5b28db91bf1eeb37b04d769eda28c85980a6df5fa37" "e266d44fa3b75406394b979a3addc9b7f202348099cfde69e74ee6432f781336" "e8567ee21a39c68dbf20e40d29a0f6c1c05681935a41e206f142ab83126153ca" "f00a605fb19cb258ad7e0d99c007f226f24d767d01bf31f3828ce6688cbdeb22" "d9a28a009cda74d1d53b1fbd050f31af7a1a105aa2d53738e9aa2515908cac4c" "c95813797eb70f520f9245b349ff087600e2bd211a681c7a5602d039c91a6428" "249e100de137f516d56bcf2e98c1e3f9e1e8a6dce50726c974fa6838fbfcec6b" "6128465c3d56c2630732d98a3d1c2438c76a2f296f3c795ebda534d62bb8a0e3" "d516f1e3e5504c26b1123caa311476dc66d26d379539d12f9f4ed51f10629df3" "3c7a784b90f7abebb213869a21e84da462c26a1fda7e5bd0ffebf6ba12dbd041" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "b7e460a67bcb6cac0a6aadfdc99bdf8bbfca1393da535d4e8945df0648fa95fb" "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "0b3aee906629ac7c3bd994914bf252cf92f7a8b0baa6d94cb4dfacbd4068391d" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "4b0e826f58b39e2ce2829fab8ca999bcdc076dec35187bf4e9a4b938cb5771dc" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "fb83a50c80de36f23aea5919e50e1bccd565ca5bb646af95729dc8c5f926cbf3" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "6b1abd26f3e38be1823bd151a96117b288062c6cde5253823539c6926c3bb178" "4133d2d6553fe5af2ce3f24b7267af475b5e839069ba0e5c80416aa28913e89a" default))
+ '(exwm-floating-border-color "#413a3a")
+ '(fci-rule-color "#615959")
+ '(highlight-tail-colors ((("#38372d") . 0) (("#343735") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#131313" "#f9cc6c"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#131313" "#adda78"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#131313" "#5b5353"))
+ '(objed-cursor-color "#fd6883")
+ '(pdf-view-midnight-colors (cons "#fff1f3" "#2c2525"))
+ '(rustic-ansi-faces
+   ["#2c2525" "#fd6883" "#adda78" "#f9cc6c" "#85dacc" "#85dacc" "#85dacc" "#fff1f3"])
+ '(vc-annotate-background "#2c2525")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#adda78")
+    (cons 40 "#c6d574")
+    (cons 60 "#dfd070")
+    (cons 80 "#f9cc6c")
+    (cons 100 "#f7b76d")
+    (cons 120 "#f4a26e")
+    (cons 140 "#f38d70")
+    (cons 160 "#cea68e")
+    (cons 180 "#a9c0ad")
+    (cons 200 "#85dacc")
+    (cons 220 "#adb4b3")
+    (cons 240 "#d58e9b")
+    (cons 260 "#fd6883")
+    (cons 280 "#d46276")
+    (cons 300 "#ac5d6a")
+    (cons 320 "#83585f")
+    (cons 340 "#615959")
+    (cons 360 "#615959")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
