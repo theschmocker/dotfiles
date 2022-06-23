13d7989ab739ebf3e42b78efd09ee4a72cff6cae
theschmocker <jacobcs1996@gmail.com>
2022-06-22 19:30:42 -0500

    removes CocAction binding from gh

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -2,7 +2,6 @@ local map = vim.api.nvim_set_keymap
 
 map('n', ';', '<Plug>Lightspeed_;_ft', { noremap = false })
 map('n', ',', '<Plug>Lightspeed_,_ft', { noremap = false })
-map('n', 'gh', "<cmd>call CocAction('doHover')<CR>", { noremap = true })
 map('n', 'Y', "y$", { noremap = true })
 map('i', 'jk', '<esc>', { noremap = true })
 map('t', 'jk', '<c-\\><c-n>', { noremap = true })
