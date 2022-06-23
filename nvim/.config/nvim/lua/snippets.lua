62547649da22f13ca03c6a897f690a6140346778
theschmocker <jacobcs1996@gmail.com>
2022-06-22 19:27:56 -0500

    swap snippet prev/next bindings

--- nvim/.config/nvim/lua/snippets.lua
+++ nvim/.config/nvim/lua/snippets.lua
@@ -26,15 +26,15 @@ ls.add_snippets("all", {
 	})
 })
 
-vim.keymap.set({ "i", "s" }, "<c-k>", function()
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
   if ls.expand_or_jumpable() then
     ls.expand_or_jump()
   end
 end, { silent = true })
 
--- <c-j> is my jump backwards key.
+-- <c-k> is my jump backwards key.
 -- this always moves to the previous item within the snippet
-vim.keymap.set({ "i", "s" }, "<c-j>", function()
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
   if ls.jumpable(-1) then
     ls.jump(-1)
   end
