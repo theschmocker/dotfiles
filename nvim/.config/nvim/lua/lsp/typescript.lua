eccba04207742737626f468a5b322bc52619be59
theschmocker <jacobcs1996@gmail.com>
2022-06-27 23:21:03 -0500

    enable svelte plugin globally in tsserver init

--- nvim/.config/nvim/lua/lsp/typescript.lua
+++ nvim/.config/nvim/lua/lsp/typescript.lua
@@ -22,6 +22,10 @@ local function is_vue_project(p_json)
 	return p_json ~= nil and p_json.dependencies ~= nil and p_json.dependencies.vue ~= nil
 end
 
+local function get_global_node_modules_path()
+	-- TODO: is there a better way to call out to npm AND/OR a better way to get the global node_modules dir
+	return string.gsub(vim.fn.system('npm root -g'), '\n', '')
+end
 
 return {
 	-- In Vue projects, I want to use Volar's Takeover mode for JS/TS files. Otherwise, I want to use tsserver.
@@ -45,6 +49,18 @@ return {
 		require'lspconfig'.tsserver.setup{
 			capabilities = config.capabilities,
 			on_attach = config.on_attach,
+			-- TODO: I wonder how I could hook into init_options more dynamically. There doesn't seem to be an issue if the plugin isn't installed, but it'd be interesting to somehow extract
+			-- this to a plugin. Maybe one that handled registering the TS plugin automatically in a Svelte workspace, installing it, etc. like the VSCode plugin. VSCode handles TS plugins
+			-- in a special way
+			init_options = {
+				plugins = {
+					{
+						name = "typescript-svelte-plugin",
+						-- TODO: is there a better way to join paths, a la node's path.join?
+						location = get_global_node_modules_path() .. '/typescript-svelte-plugin'
+					}
+				}
+			},
 			root_dir = function (filepath)
 				if not is_vue_project(get_package_json()) then
 					return default_root_dir(filepath)
