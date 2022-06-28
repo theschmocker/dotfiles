c080410c69f40041a986007c17d564f808aab74d
theschmocker <jacobcs1996@gmail.com>
2022-06-28 17:45:45 -0500

    workaround for document version issue in volar (buffer newer than edits)

--- nvim/.config/nvim/lua/lsp/typescript.lua
+++ nvim/.config/nvim/lua/lsp/typescript.lua
@@ -34,6 +34,17 @@ return {
 	setup = function (config)
 		local default_root_dir = util.root_pattern('package.json')
 
+		-- temporary fix for volar's document version issue
+		vim.lsp.util.apply_text_document_edit = function(text_document_edit, index, offset_encoding)
+			local text_document = text_document_edit.textDocument
+			local bufnr = vim.uri_to_bufnr(text_document.uri)
+			if offset_encoding == nil then
+				vim.notify_once('apply_text_document_edit must be called with valid offset encoding', vim.log.levels.WARN)
+			end
+
+			vim.lsp.util.apply_text_edits(text_document_edit.edits, bufnr, offset_encoding)
+		end
+
 		require'lspconfig'.volar.setup{
 			capabilities = config.capabilities,
 			on_attach = config.on_attach,
