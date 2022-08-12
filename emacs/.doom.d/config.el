2f52dc1931ea3d5b8bc6a7520c2b39f03cea27b4
theschmocker <jacobcs1996@gmail.com>
2022-08-12 11:36:34 -0500

    limit modes in which I enable cleverparens

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -98,7 +98,10 @@
 
 ;;; Cleverparens
 (setq evil-cleverparens-use-s-and-S nil)
-(add-hook 'smartparens-mode-hook #'evil-cleverparens-mode)
+(add-hook 'emacs-lisp-mode-hook #'evil-cleverparens-mode)
+(add-hook 'lisp-mode-hook #'evil-cleverparens-mode)
+(add-hook 'clojure-mode-hook #'evil-cleverparens-mode)
+(add-hook 'scheme-mode-hook #'evil-cleverparens-mode)
 
 ;;; LSP
 (after! lsp-mode
