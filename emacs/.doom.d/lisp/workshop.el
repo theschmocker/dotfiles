f50829484c88305cc0a084533e0a58eb0c08d73c
theschmocker <jacobcs1996@gmail.com>
2023-07-31 23:52:13 -0500

    emacs: workshop some super hacky functions to partially automate conversion of c# types to typescript types

--- emacs/.doom.d/lisp/workshop.el
+++ emacs/.doom.d/lisp/workshop.el
@@ -87,6 +87,53 @@ Name is read from the package.json file."
 ;;   (dlet ((default-directory (file-name-directory (schmo/project-package-json-completing-read))))
 ;;     (call-interactively #'npm-mode-npm-run)))
 
+(defun schmo/cleanup-types-for-translation (start end)
+  (interactive (if (region-active-p)
+                   (list (region-beginning) (region-end))
+                 (list (point-min) (point-max))))
+  (replace-regexp "public enum" "export enum" nil start end)
+  (replace-regexp "public\\( abstract\\)? class" "export interface" nil start end)
+  (replace-regexp "\\(\\W+\\)public ?\\(override\\|abstract\\)? " "\\1" nil start end)
+  (replace-regexp "{\\(.*?\\(get;\\|set;\\)+.*?\\)+}" "" nil start end)
+  (replace-regexp "bool" "boolean" t start end)
+  (replace-regexp "\\(int\\|decimal\\|double\\)" "number" t start end)
+  (replace-regexp "DateTime" "string" t start end)
+  (replace-regexp "Dictionary" "Record" t start end)
+  (replace-regexp "{ ?get; ?set; ?}.*$" "" nil start end)
+  (delete-trailing-whitespace))
+
+(defun schmo/convert-normal-property ()
+  (interactive)
+  (end-of-line)
+  (backward-kill-word 1)
+  (back-to-indentation)
+  (yank)
+  (insert ": ")
+  (set-mark (point))
+  (end-of-line)
+  (delete-trailing-whitespace)
+  (deactivate-mark)
+  (insert ";")
+  (schmo/downcase-first-char)
+  (forward-line 1))
+
+(defun schmo/downcase-first-char ()
+  (save-excursion
+    (back-to-indentation)
+    (downcase-region (point) (1+ (point)))))
+
+(defun schmo/convert-discriminant-property ()
+  (interactive)
+  (let ((start (save-excursion
+                 (back-to-indentation)
+                 (point)))
+        (end (save-excursion
+               (end-of-line)
+               (point))))
+    (replace-regexp "\\(\\w+?\\) \\(\\w+?\\) => \\(.+?\\);" "\\2: \\3;" nil start end)
+    (schmo/downcase-first-char)
+    (goto-char start)
+    (forward-line 1)))
 (provide 'workshop)
 
 ;;; workshop.el ends here
