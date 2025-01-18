3390797181a63b6c99e2991c868b7c68137a70b6
theschmocker <jacobcs1996@gmail.com>
2025-01-18 12:08:14 -0600

    nvim: tweak completeopt for nvim-cmp

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -47,7 +47,7 @@ return {
 								luasnip.lsp_expand(args.body)
 							end,
 						},
-						completion = { completeopt = 'menu,menuone,noinsert' },
+						completion = { completeopt = 'menu,menuone,noinsert,noselect' },
 
 						-- For an understanding of why these mappings were
 						-- chosen, you will need to read `:help ins-completion`
