ec3a0fce019da55d178c20b82e44bd61bc4a1ec2
theschmocker <jacobcs1996@gmail.com>
2025-02-21 00:11:51 -0600

    nvim: statusline tweaks

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -30,7 +30,37 @@ local function setup_mini_statusline()
 	--  and try some other statusline plugin
 	local statusline = require 'mini.statusline'
 	-- set use_icons to true if you have a Nerd Font
-	statusline.setup { use_icons = vim.g.have_nerd_font }
+	local content = function()
+		local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+		local git           = MiniStatusline.section_git({ trunc_width = 40 })
+		local diff          = vim.trim(MiniStatusline.section_diff({ trunc_width = 75, icon = '' }))
+		if diff == "-" then
+			diff = ""
+		end
+		local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+		local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+		local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+		local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+		local location      = MiniStatusline.section_location({ trunc_width = 75 })
+		local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+		return MiniStatusline.combine_groups({
+			{ hl = mode_hl,                  strings = { mode } },
+			{ hl = 'MiniStatuslineDevinfo',  strings = { git, diff } },
+			'%<', -- Mark general truncate point
+			{ hl = 'MiniStatuslineFilename', strings = { filename } },
+			'%=', -- End left alignment
+			{ hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+			{ hl = mode_hl,                  strings = { search, location } },
+		})
+	end
+	statusline.setup {
+		use_icons = vim.g.have_nerd_font,
+		content = {
+			active = content,
+			inactive = content,
+		}
+	}
 
 	-- You can configure sections in the statusline by overriding their
 	-- default behavior. For example, here we set the section for
