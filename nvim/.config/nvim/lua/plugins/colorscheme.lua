31887197832457093793017dea101e7e3a08dec2
theschmocker <jacobcs1996@gmail.com>
2025-05-30 23:16:16 -0500

    nvim: some todos/ideas

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -33,6 +33,14 @@ return {
 						DiagnosticSignError = { fg = '#D52323' },
 						DiagnosticUnderlineInfo = { underdotted = true },
 						["@string.special.url"] = { undercurl = false, underdotted = true },
+						-- TODO: see if there's a way to just darken the normal highlights
+						-- for DiagnosticUnnecessary, instead of making it fully gray
+						-- potential leads
+						-- for creating highlights based on another and modifying its colors
+						-- https://www.reddit.com/r/neovim/comments/qco76a/comment/hhiiuww/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+						--  plus
+						-- for the darkening-into-the-background/transparency-like effect I want
+						-- the blending algorithm this site uses https://meyerweb.com/eric/tools/color-blend/
 
 						-- indent-blankline overrides
 						IblIndent = { fg = colors.palette.dragonBlack4 },
