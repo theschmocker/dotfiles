5b5c415e8bf06b69fdc452757399d075240d7afb
theschmocker <jacobcs1996@gmail.com>
2025-08-07 21:29:02 -0500

    remove rose pine

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,5 @@
 return {
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("rose-pine").setup({
-				dark_variant = "moon",
-			})
-			-- vim.cmd("colorscheme rose-pine")
-		end
-	},
-	{
 		"rebelot/kanagawa.nvim",
 		config = function()
 			require('kanagawa').setup({
