eedd5967860cc4bcd996fcf5bfb59dbb9e0a3a4d
theschmocker <jacobcs1996@gmail.com>
2026-04-12 08:47:25 -0500

    nvim: restore pre-yank cursor position; wip mouse scroll scrolloff

--- nvim/.config/nvim/lua/config/settings.lua
+++ nvim/.config/nvim/lua/config/settings.lua
@@ -68,3 +68,49 @@ vim.diagnostic.config({
 -- silence deprecation warnings
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.deprecate = function() end
+
+-- TODO: this breaks mouse scroll in normal mode when at bottom (gets stuck)
+-- vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "WinScrolled" }, {
+-- 	desc = "Fix scrolloff when you are at the EOF",
+-- 	group = vim.api.nvim_create_augroup("ScrollEOF", { clear = true }),
+-- 	callback = function()
+-- 		local is_terminal = vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "terminal"
+-- 		if vim.api.nvim_win_get_config(0).relative ~= "" or is_terminal then
+-- 			return -- Ignore floating windows
+-- 		end
+--
+-- 		local win_height = vim.fn.winheight(0)
+-- 		local scrolloff = math.min(vim.o.scrolloff, math.floor(win_height / 2))
+-- 		local visual_distance_to_eof = win_height - vim.fn.winline()
+--
+-- 		if visual_distance_to_eof < scrolloff then
+-- 			local win_view = vim.fn.winsaveview()
+-- 			vim.fn.winrestview({ topline = win_view.topline + scrolloff - visual_distance_to_eof })
+-- 		end
+-- 	end,
+-- })
+
+local keep_pre_yank_cursor_pos = function ()
+	vim.keymap.set({ "n", "x" }, "y", function()
+		vim.b.user_cursor_pre_yank = vim.api.nvim_win_get_cursor(0)
+		return "y"
+	end, { expr = true })
+
+	vim.keymap.set("n", "Y", function()
+		vim.b.user_cursor_pre_yank = vim.api.nvim_win_get_cursor(0)
+		return "y$"
+	end, { expr = true })
+
+	local group = vim.api.nvim_create_augroup("keep_pre_yank_cursor_pos", { clear = true })
+	vim.api.nvim_create_autocmd("TextYankPost", {
+		group = group,
+		callback = function()
+			if vim.v.event.operator == "y" and vim.b.user_cursor_pre_yank then
+				vim.api.nvim_win_set_cursor(0, vim.b.user_cursor_pre_yank)
+				vim.b.user_cursor_pre_yank = nil
+			end
+		end,
+	})
+end
+
+keep_pre_yank_cursor_pos()
